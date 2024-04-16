53595d4cd7e7678b0e8b0bd5b3e4b648e6b5e5c7
direct66612 <113045624+direct66612@users.noreply.github.com>
2024-04-16 02:32:40 -0400

    init

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -16,8 +16,8 @@ export default function Page({ params }: PageProps) {
     }
   }, [params.id]);
   return (
-    <>
-      <Header>Company ({params.id})</Header>
-    </>
+    <div className="py-6 px-10">
+      <p>{`Information about company (${params.id})`}</p>
+    </div>
   );
 }
